ea44d03598c9f4d6580ad988fbe0f3c209335cd3
GaneshNK-2003 <ganeshnk2003@gmail.com>
2025-04-01 20:16:36 +0530

    setup basic project files

---
+++ lib/screens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+        actions: [IconButton(onPressed: (){}, icon: Icon(Icons.settings))],
+      ),
+      body: Column(
+        children: [
+            R
+        ],
+      ),
+    );
+  }
+}
